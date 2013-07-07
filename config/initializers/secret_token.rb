07e51dbaf96293880bf5d2063699801df36b5bc9
Emcm29 <Emcm29@gmail.com>
2013-07-07 14:54:23 +0100

    Testing devise-mongoid + puma on ey

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyTest::Application.config.secret_token = '0832343dffbb14fc152bfa49ac271166b3f78f3dcfa5655b95924ef793c2d48ee038951c21b302e571b74f873674327169350f8849667a6ef448f82ee4cefa54'
